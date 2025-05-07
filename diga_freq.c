51cd93f5e892418c12d3dcd2b8d2f058a20a53b7
marcelinhor <mareginato31@gmail.com>
2025-05-07 15:06:40 -0300

    commit inicial

---
+++ diga_freq.c
@@ -0,0 +1,12 @@
+/*
+Giovanni dos Santos 13695341
+Guilherme Augusto Fincatti da Silva 13676986
+Marcelo Eduardo Reginato 13676965
+Pedro Guilherme de Barros Zenatte 13676919
+*/
+
+
+int main(){
+
+    return 0;
+}
